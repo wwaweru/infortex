ef641e9c2a87f5ca3486e3d13f014046aacd0f1e
jijothic <george.jijothic@gmail.com>
2025-11-25 13:35:11 +0300

    updated about contact faq index services.html

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 $subject = 'New Contact Message'; // Subject of your email
-$to = 'contact@designesia.com';  //Recipient's E-mail
+$to = 'contact@infotexsols.com';  //Recipient's E-mail
 
 $emailTo = $_POST['email'];
 $name = $_POST['name'];
